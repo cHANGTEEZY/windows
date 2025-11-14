01a04d77abb89021a2c3cc1acf6a9a7513aaa74e
cHANGTEEZY <jagatgurung30@gmail.com>
2025-11-14 17:09:55 +0545

    initial commit

---
+++ lib/auth.ts
@@ -0,0 +1,11 @@
+import { betterAuth } from "better-auth";
+import { Pool } from "pg";
+
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: { rejectUnauthorized: false },
+});
+
+export const auth = betterAuth({
+  database: pool,
+});
