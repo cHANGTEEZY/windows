bb7b4f35e5b0b52393024bf760aa47e480179734
cHANGTEEZY <jagatgurung30@gmail.com>
2025-12-03 19:48:45 +0545

    fix( infinite auth loop)

--- proxy.ts
+++ proxy.ts
@@ -7,8 +7,9 @@ export async function proxy(request: NextRequest) {
   //! THIS IS NOT SECURE!
   //? This is the recommended approach to optimistically redirect users
   //? We recommend handling auth checks in each page/route
+
   if (!sessionCookie) {
-    return NextResponse.redirect(new URL("/", request.url));
+    return NextResponse.redirect(new URL("/signin", request.url));
   }
 
   return NextResponse.next();
