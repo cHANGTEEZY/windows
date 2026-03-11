bb6bb9e6356144b62ae8b32e70dd0bf0f6930561
cHANGTEEZY <jagatgurung30@gmail.com>
2026-03-11 18:05:42 +0545

    add lockscreen

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import "../app-menu-with-lock-screen/dist/style.css";
+import "./lockscreen.scss";
+
 import { Toaster } from "@/components/ui/sonner";
 
 const geistSans = Geist({
